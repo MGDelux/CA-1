06c58ed91db1b34fc5ba30ec2e67cc084aec51e5
MGDelux <mathiastouboel@gmail.com>
2021-02-24 13:39:48 +0100

    test

--- src/test/java/rest/RenameMeResourceTest.java
+++ src/test/java/rest/RenameMeResourceTest.java
@@ -9,9 +9,7 @@ import java.net.URI;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.ws.rs.core.UriBuilder;
-import org.glassfish.grizzly.http.server.HttpServer;
-import org.glassfish.grizzly.http.util.HttpStatus;
-import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.apache.http.impl.bootstrap.HttpServer;
 import org.glassfish.jersey.server.ResourceConfig;
 import static org.hamcrest.Matchers.equalTo;
 import org.junit.jupiter.api.AfterAll;
@@ -20,7 +18,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 //Uncomment the line below, to temporarily disable this test
-//@Disabled
+@Disabled
 
 public class RenameMeResourceTest {
 
@@ -32,10 +30,7 @@ public class RenameMeResourceTest {
     private static HttpServer httpServer;
     private static EntityManagerFactory emf;
 
-    static HttpServer startServer() {
-        ResourceConfig rc = ResourceConfig.forApplication(new ApplicationConfig());
-        return GrizzlyHttpServerFactory.createHttpServer(BASE_URI, rc);
-    }
+
 
     @BeforeAll
     public static void setUpClass() {
@@ -43,7 +38,6 @@ public class RenameMeResourceTest {
         EMF_Creator.startREST_TestWithDB();
         emf = EMF_Creator.createEntityManagerFactoryForTest();
 
-        httpServer = startServer();
         //Setup RestAssured
         RestAssured.baseURI = SERVER_URL;
         RestAssured.port = SERVER_PORT;
@@ -73,7 +67,7 @@ public class RenameMeResourceTest {
 
         //Don't forget this, if you called its counterpart in @BeforeAll
         EMF_Creator.endREST_TestWithDB();
-        httpServer.shutdownNow();
+    
     }
 
     // Setup the DataBase (used by the test-server and this test) in a known state BEFORE EACH TEST
